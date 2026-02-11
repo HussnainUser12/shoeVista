f550463ad33ed9ebb9cb94e292908768e5bf2ea8
Hussnain <userlearn8866@gmail.com>
2026-02-12 01:42:53 +0500

    Submit Test

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,14 @@ import ShopBy from "../components/ShopBy";
 import GenInfo, { Brands } from "../components/GenInfo";
 
 const Home = () => {
+  
+  const topBrandsProducts = [
+    { name: "adidas", src:"/GenInfo/adidas.jpg", _id: "adidas" },
+    { name: "Nikes", src:"/GenInfo/nikes.jpg", _id: "nikes" },
+    { name: "Skechers", src:"/GenInfo/skechers.jpg", _id: "skechers" },
+    { name: "Puma", src:"/GenInfo/puma.jpg", _id: "puma" },
+  ];
+  
   return (
     <div className="max-w-screen-xl xs:w-[95vw] xs:max-w-[95vw] md:w-full mx-auto ">
       <Carousel />
@@ -11,7 +19,7 @@ const Home = () => {
       <Brands />
       <div className="md:w-full md:max-w-full xs:mx-2  sm:mx-auto ">
         <div className="prose prose-2xl">
-          <ShopBy title="Best Sellers" filter="bestSellers" />
+          <ShopBy title="Best Sellers" filter="bestSellers" staticData={topBrandsProducts} />
         </div>
         <div className="prose prose-2xl">
           <ShopBy title="Top Rated" filter="topRated" />
